0718cb44189414f0a47e1f8fdcc45384b691be49
Tyler King <doitliketyler@gmail.com>
2022-12-17 14:06:52 -0800

    Removed unused link module

--- frontend/src/components/Articles.tsx
+++ frontend/src/components/Articles.tsx
@@ -1,5 +1,4 @@
 import { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
